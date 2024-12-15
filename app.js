123983b2c90b32184087cec89084425ac4da2cd5
rohan <rohankhan08453@gmail.com>
2024-12-15 09:53:10 +0500

    .

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 let city = document.querySelector('#city')
 let card = document.querySelector('.card')
+let Right = document.querySelector('.right')
+let left = document.querySelector('.left')
 function getWeather(){
 fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&appid=392fc470c1ac8b42b2f40951a9a96cc4&units=metric`)
@@ -10,20 +12,20 @@ fetch(
     console.log("Data-->",data);
     console.log(data.main.temp);
     
-    card.innerHTML = `
-  <h1>WEATHER FORECAST <br> <span>${data.name}</span> </h1>
+    left.innerHTML = `
+ 
     <h2 class = "temp">${Math.round(data.main.temp)}°c  ${data.weather[0].main}</h2>
     <h3> feels like ${Math.round(data.main.feels_like)}°c</h3>
       <h3> Humidity ${Math.round(data.main.humidity)}°c</h3>
       <h3> Wind Speed ${(data.wind.speed)}°c</h3>`
       if (data.weather[0].main === "Clear") {
-        card.innerHTML += `<img id="right" src="https://cdn-icons-png.freepik.com/256/1249/1249115.png" alt="Sunny" width="60px" height="60px">`;
+        Right.innerHTML += `<img id="right" src="https://cdn-icons-png.freepik.com/256/1249/1249115.png" alt="Sunny" width="120px" height="120px">`;
       } else if (data.weather[0].main === "Rain") {
-        card.innerHTML += `<img id="right" src="https://cdn-icons-png.freepik.com/256/9116/9116642.png?ga=GA1.1.1507680453.1719128961&semt=ais_hybrid" alt="Rain" width="60px" height="60px">`;
+        Right.innerHTML += `<img id="right" src="https://cdn-icons-png.freepik.com/256/9116/9116642.png?ga=GA1.1.1507680453.1719128961&semt=ais_hybrid" alt="Rain" width="120px" height="120px">`;
       } else if (data.weather[0].main === "Clouds") {
-        card.innerHTML += `<img id="right" src="https://cdn-icons-png.freepik.com/256/14855/14855054.png?ga=GA1.1.1507680453.1719128961&semt=ais_hybrid alt="Clouds" width="60px" height="60px">`;
+        Right.innerHTML += `<img id="right" src="https://cdn-icons-png.freepik.com/256/14855/14855054.png?ga=GA1.1.1507680453.1719128961&semt=ais_hybrid alt="Clouds" width="120px" height="120px">`;
       } else {
-        card.innerHTML += `<img id="right" src="https://cdn-icons-png.freepik.com/256/3026/3026315.png?ga=GA1.1.1507680453.1719128961&semt=ais_hybrid" alt="Default Weather" width="60px" height="60px">`;
+        Right.innerHTML += `<img id="right" src="https://cdn-icons-png.freepik.com/256/3026/3026315.png?ga=GA1.1.1507680453.1719128961&semt=ais_hybrid" alt="Default Weather" width="120px" height="120px">`;
       }
 })
 .catch(function(err){
